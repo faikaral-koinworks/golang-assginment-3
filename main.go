b08e3d55e787a7621123305fb247281138aa22ea
faluthfi <faikar.al@gmail.com>
2022-03-21 14:15:01 +0700

    working commit

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 var wind, water int
+var isSent bool = true
+
+var upgrader=websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:func(r *http.Request)bool{
+		return true
+	},
+}
 
 func main() {
 	go reroll()
@@ -19,10 +29,15 @@ func main() {
 		"water": &water,
 	}
 
-	server.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusAccepted, gin.H{
+	server.GET("/json", func(c *gin.Context) {
+		c.JSON(http.StatusAccepted, gin.H{
 			"status": data,
 		})
+	
+	})
+	server.GET("/ws",wsEndPoint)
+	server.GET("/",func(c *gin.Context) {
+		c.File("./index.html")
 	})
 
 	server.Run(":8080")
@@ -33,8 +48,37 @@ func reroll() {
 	for {
 		wind = rand.Intn(100)
 		water = rand.Intn(100)
-		fmt.Println(wind)
-		fmt.Println(water)
-		time.Sleep(5 * time.Second)
+		isSent=false
+		time.Sleep(15*time.Second)
 	}
+}
+
+
+func wsEndPoint(c *gin.Context){
+	ws,err:=upgrader.Upgrade(c.Writer,c.Request,nil)
+	if err != nil{
+		panic(err)
+	}
+
+	data := map[string]interface{}{
+		"wind":  &wind,
+		"water": &water,
+	}
+
+	defer ws.Close()
+
+	for {
+		for isSent==false{
+			err:=ws.WriteJSON(data)
+			if err != nil{
+				panic(err)
+			}
+			isSent=true
+		}
+		if err !=nil{
+			log.Fatal(err)
+		}
+	}
+
+
 }
